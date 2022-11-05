bd1d5cd25251fbef7f7a3a9dbeff0675325f32a5
Eray <86014603+eraybtgn@users.noreply.github.com>
2022-11-05 21:21:30 +0300

    Add files via upload

---
+++ Suggestion/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int heat;
+        System.out.println("Hava durumuna göre öneri proramına hoşgeldiniz.");
+        System.out.println("Bugün hava nasıl? ");
+        heat=scan.nextInt();
+        if (heat<5){
+            System.out.println("Hava baya soğuk tam kayak yapmalıkkk.");
+        } else if (heat>5 && heat<15) {
+            System.out.println("Bugün çok ılık ve yumuşak bir hava var hadi sinamaya gidelim.");
+        } else if (heat>15 && heat<25) {
+            System.out.println("Bu hava tam piknik havası.");
+        } else if (heat>25) {
+            System.out.println("Hava baya sıcak hadi denize gidelim.");
+        }
+
+    }
+}
