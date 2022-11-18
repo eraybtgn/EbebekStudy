ba86265c992d77a41092c3a82f80bbfe681cff31
Eray <86014603+eraybtgn@users.noreply.github.com>
2022-11-18 17:42:21 +0300

    Add files via upload

--- DiziOrt/src/Main.java
+++ DiziOrt/src/Main.java
@@ -1,23 +1,37 @@
-public class Main {
-    public static void main(String[] args) {
-        String[][] list=new String[7][4];
+import java.util.Arrays;
 
-        for (int i=0; i<list.length; i++){
-            for (int j=0; j<list[i].length; j++){
-                if (i==0 || i==3 || i==6){
-                    list[i][j]=" * ";
-                } else if (j==0 || j==3) {
-                    list[i][j]=" * ";
-                }else {
-                    list[i][j]="   ";
+public class DizidekiTekrarEdenCiftSayi {
+    static boolean isFind(int[] arr, int value){
+        for (int i:arr){
+            if (i==value){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[]args){
+        int [] list={3,4,4,5,6,6,7,8,8};
+        System.out.println("Dizi : " + Arrays.toString(list));
+        int duplicate []=new int[list.length];
+        int startIndex=0;
+        for(int i=0; i<list.length; i++){
+            for (int j=0; j<list.length; j++){
+                if (i!=j && list[i]==list[j]){
+                    if (list[i]%2==0){
+                        if (!isFind(duplicate,list[i])){
+                            duplicate[startIndex++]=list[i];
+                        }
+                        break;
+                    }
                 }
             }
         }
-        for (String[] row:list ){
-            for (String col:row){
-                System.out.print(col);
+        System.out.print("Tekrar Eden Çift Sayılar: ");
+        for (int value : duplicate){
+            if (value!=0){
+                System.out.print(value+" ");
             }
-            System.out.println();
 
+        }
     }
 }
